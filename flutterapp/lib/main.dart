739dcf9e6d13a2e2b00e7d4a08e96faadff8a1d8
LuisJurado99 <luisjurado9924@gmai.com>
2020-11-06 21:54:38 -0600

    Se termina la app

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -4,68 +4,43 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Aiuda'),
-    );
-  }
+  State<StatefulWidget> createState() => MyHomePageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
+class MyHomePageState extends State {
+  double opacidad = 1.0;
 
   @override
-  Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: TextEditingController(),
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter your email';
-                }
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Hola"),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                setState(() {
+                  if (opacidad > 0.1) {
+                    opacidad = opacidad - 0.1;
+                  } else {
+                    opacidad = 1.0;
+                  }
+                });
               },
             ),
-            TextFormField(
-              controller: TextEditingController(),
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter your password';
-                }
-              },
-            ),
-            RaisedButton(
-              child: Text('Login'),
-            ),
-            RaisedButton(
-              child: Text('SignUp'),
-            ),
-            RaisedButton(child: Text("Pagina Internet"),)
-          ],
-        ),
-      ),
-    );
-  }
+            body: Row(
+              children: [
+                Opacity(
+                  child: Image.asset(
+                    "images/message.png",
+                    width: 200,
+                  ),
+                  opacity: opacidad,
+                ),
+                Text("La opacidad es $opacidad")
+              ],
+            )),
+      );
 }
